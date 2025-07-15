8d055163cdacd6a29e703829778b214dbc69106d
matefernandezcc <matefernandezc@gmail.com>
2025-07-15 01:12:15 +0000

    Style 🎨: Some Images

--- dinosaurs.js
+++ dinosaurs.js
@@ -1,83 +1,83 @@
-// Datos de dinosaurios con sus ubicaciones
 const dinosaurs = [
-  {
-    id: 1,
-    name: "Tyrannosaurus rex",
-    image: "https://upload.wikimedia.org/wikipedia/commons/1/12/Tyrannosaurus_rex_MHNT.PAL_.2005.0.10.jpg",
-    location: { lat: 43.0, lng: -107.5 },
-    region: "América del Norte",
-    info: "Vivió en el Cretácico tardío, hace unos 68-66 millones de años. Era un depredador feroz de hasta 12 metros de longitud."
-  },
-  {
-    id: 2,
-    name: "Velociraptor",
-    image: "https://upload.wikimedia.org/wikipedia/commons/0/07/Velociraptor_mongoliensis_skull.jpg",
-    location: { lat: 43.6, lng: 104.3 },
-    region: "Mongolia",
-    info: "Vivió en el período Cretácico hace unos 75-71 millones de años. Era un dinosaurio pequeño pero ágil y depredador."
-  },
-  {
-    id: 3,
-    name: "Argentinosaurus",
-    image: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Argentinosaurus_DB.jpg",
-    location: { lat: -38.9, lng: -69.2 },
-    region: "Patagonia, Argentina",
-    info: "Uno de los dinosaurios más grandes que existió. Vivió durante el Cretácico medio, hace unos 97-93 millones de años."
-  },
-  {
-    id: 4,
-    name: "Stegosaurus",
-    image: "https://upload.wikimedia.org/wikipedia/commons/6/62/Stegosaurus_Skeleton_Holotype.JPG",
-    location: { lat: 39.1, lng: -105.3 },
-    region: "América del Norte",
-    info: "Vivió durante el período Jurásico tardío, hace unos 155-150 millones de años. Conocido por las placas en su espalda."
-  },
-  {
-    id: 5,
-    name: "Triceratops",
-    image: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Triceratops_BW.jpg",
-    location: { lat: 45.7, lng: -104.5 },
-    region: "América del Norte",
-    info: "Vivió al final del período Cretácico, hace unos 68-66 millones de años. Tenía tres cuernos distintivos en su cabeza."
-  },
-  {
-    id: 6,
-    name: "Spinosaurus",
-    image: "https://upload.wikimedia.org/wikipedia/commons/3/32/Spinosaurus_Dragon.JPG",
-    location: { lat: 31.8, lng: 9.8 },
-    region: "Norte de África",
-    info: "Vivió durante el Cretácico medio, hace unos 112-97 millones de años. Tenía una gran vela en su espalda y era semiacuático."
-  },
-  {
-    id: 7,
-    name: "Brachiosaurus",
-    image: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Brachiosaurus_BW.jpg",
-    location: { lat: 39.3, lng: -107.2 },
-    region: "América del Norte",
-    info: "Vivió durante el Jurásico tardío, hace unos 154-153 millones de años. Tenía un cuello extremadamente largo."
-  },
-  {
-    id: 8,
-    name: "Ankylosaurus",
-    image: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Ankylosaurus_Museum_of_the_Rockies.jpg",
-    location: { lat: 50.5, lng: -110.0 },
-    region: "América del Norte",
-    info: "Vivió al final del período Cretácico, hace unos 68-66 millones de años. Tenía una cola con un mazo óseo."
-  },
-  {
-    id: 9,
-    name: "Parasaurolophus",
-    image: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Parasaurolophus_skeleton_01.jpg",
-    location: { lat: 51.0, lng: -113.5 },
-    region: "América del Norte",
-    info: "Vivió durante el Cretácico tardío, hace unos 76-74 millones de años. Tenía una cresta tubular en la cabeza."
-  },
-  {
-    id: 10,
-    name: "Allosaurus",
-    image: "https://upload.wikimedia.org/wikipedia/commons/e/e7/Allosaurus_skeleton.jpg",
-    location: { lat: 38.5, lng: -106.9 },
-    region: "América del Norte",
-    info: "Vivió durante el Jurásico tardío, hace unos 155-145 millones de años. Era uno de los principales depredadores de su época."
-  }
-]; 
+    {
+      id: 1,
+      name: "Tyrannosaurus rex",
+      image: "assets/tyrannosaurus.png",
+      location: { lat: 43.0, lng: -107.5 },
+      region: "América del Norte",
+      info: "Vivió en el Cretácico tardío, hace unos 68-66 millones de años. Era un depredador feroz de hasta 12 metros de longitud."
+    },
+    {
+      id: 2,
+      name: "Velociraptor",
+      image: "assets/velociraptor.webp",
+      location: { lat: 43.6, lng: 104.3 },
+      region: "Mongolia",
+      info: "Vivió en el período Cretácico hace unos 75-71 millones de años. Era un dinosaurio pequeño pero ágil y depredador."
+    },
+    {
+      id: 3,
+      name: "Argentinosaurus",
+      image: "assets/argentinosaurus.webp",
+      location: { lat: -38.9, lng: -69.2 },
+      region: "Patagonia, Argentina",
+      info: "Uno de los dinosaurios más grandes que existió. Vivió durante el Cretácico medio, hace unos 97-93 millones de años."
+    },
+    {
+      id: 4,
+      name: "Stegosaurus",
+      image: "assets/stegosaurus.png",
+      location: { lat: 39.1, lng: -105.3 },
+      region: "América del Norte",
+      info: "Vivió durante el período Jurásico tardío, hace unos 155-150 millones de años. Conocido por las placas en su espalda."
+    },
+    {
+      id: 5,
+      name: "Triceratops",
+      image: "assets/triceratops.png",
+      location: { lat: 45.7, lng: -104.5 },
+      region: "América del Norte",
+      info: "Vivió al final del período Cretácico, hace unos 68-66 millones de años. Tenía tres cuernos distintivos en su cabeza."
+    },
+    {
+      id: 6,
+      name: "Spinosaurus",
+      image: "assets/spinosaurus.webp",
+      location: { lat: 31.8, lng: 9.8 },
+      region: "Norte de África",
+      info: "Vivió durante el Cretácico medio, hace unos 112-97 millones de años. Tenía una gran vela en su espalda y era semiacuático."
+    },
+    {
+      id: 7,
+      name: "Brachiosaurus",
+      image: "assets/brachiosaurus.png",
+      location: { lat: 39.3, lng: -107.2 },
+      region: "América del Norte",
+      info: "Vivió durante el Jurásico tardío, hace unos 154-153 millones de años. Tenía un cuello extremadamente largo."
+    },
+    {
+      id: 8,
+      name: "Ankylosaurus",
+      image: "assets/ankylosaurus.png",
+      location: { lat: 50.5, lng: -110.0 },
+      region: "América del Norte",
+      info: "Vivió al final del período Cretácico, hace unos 68-66 millones de años. Tenía una cola con un mazo óseo."
+    },
+    {
+      id: 9,
+      name: "Parasaurolophus",
+      image: "assets/parasaurolophus.PNG",
+      location: { lat: 51.0, lng: -113.5 },
+      region: "América del Norte",
+      info: "Vivió durante el Cretácico tardío, hace unos 76-74 millones de años. Tenía una cresta tubular en la cabeza."
+    },
+    {
+      id: 10,
+      name: "Allosaurus",
+      image: "assets/allosaurus.png",
+      location: { lat: 38.5, lng: -106.9 },
+      region: "América del Norte",
+      info: "Vivió durante el Jurásico tardío, hace unos 155-145 millones de años. Era uno de los principales depredadores de su época."
+    }
+  ];
+  
